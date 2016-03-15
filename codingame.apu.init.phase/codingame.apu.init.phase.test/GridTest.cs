b8894c3e4e513a41b9745b287043f66d772a56c6
alinddicu <alinddicu@yahoo.com>
2016-03-15 14:51:05 +0100

    added test

---
+++ codingame.apu.init.phase/codingame.apu.init.phase.test/GridTest.cs
@@ -0,0 +1,53 @@
+﻿namespace codingame.apu.init.phase.test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using NFluent;
+    
+    [TestClass]
+    public class GridTest
+    {
+        private Writter _writter;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _writter = new Writter();
+        }
+
+        [TestMethod]
+        public void BaseTest()
+        {
+            var grid = new Grid(2, 2);
+            grid.AddCells(0, "00");
+            grid.AddCells(1, "0.");
+
+            grid.WriteLines(_writter.Write);
+
+            var expected = new[] 
+            { 
+                "0 0 1 0 0 1",
+                "1 0 -1 -1 -1 -1",
+                "0 1 -1 -1 -1 -1"
+            };
+
+            Check.That(_writter.Lines).Contains(expected);
+        }
+
+        private class Writter
+        {
+            private readonly List<string> _lines = new List<string>();
+
+            public void Write(string line)
+            {
+                _lines.Add(line);
+            }
+
+            public IEnumerable<string> Lines { get { return _lines; } }
+        }
+    }
+}
